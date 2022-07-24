f6d0a0539bf993bb6ef7ebe6c26486b3eba12f73
hammad <hammadbhat126@gmail.com>
2022-07-24 14:16:01 +0400

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 // The Main Function is the starting point for all flutter apps.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Hammad'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
